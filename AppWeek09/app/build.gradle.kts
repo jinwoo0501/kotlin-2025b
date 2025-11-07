5554476e06d3c5cce8908b7334f59703060d3d40
jinwoo0501 <168405013+jinwoo0501@users.noreply.github.com>
2025-11-07 16:03:23 +0900

    week09 : Student List App v1.0

--- AppWeek09/app/build.gradle.kts
+++ AppWeek09/app/build.gradle.kts
@@ -33,23 +33,25 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    // https://developer.android.com/develop/ui/views/layout/recyclerview?hl=ko#kotlin
 
-    buildFeatures{  // ViewBinding Activate
+    // ViewBinding Activate
+    // pros : 1) 타입 안정성, 2) Null 안정성, 3) findViewByID 반복 제거, 4) 성능
+    // cons : 1) 빌드 시간 증가, 2) 바인딩 객체 생성 필요, 3) 학습 곡선
+    buildFeatures{
         viewBinding = true
     }
 }
 
 dependencies {
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.activity:activity-ktx:1.9.3")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+    implementation("androidx.recyclerview:recyclerview:1.4.0")
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
-
-    implementation("androidx.recyclerview:recyclerview:1.4.0")  // RecyclerView
-
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 }
